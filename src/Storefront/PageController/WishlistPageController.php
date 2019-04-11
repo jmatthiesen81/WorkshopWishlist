9f5e8e54b5269e25e80708930bf39ed9ec7a34e1
Philipp Beyer <p.beyer@onacy.de>
2019-04-11 10:45:31 +0200

    Detail Controller & frontend

--- src/Storefront/PageController/WishlistPageController.php
+++ src/Storefront/PageController/WishlistPageController.php
@@ -5,6 +5,7 @@ namespace Workshop\Plugin\WorkshopWishlist\Storefront\PageController;
 use Shopware\Core\Framework\Routing\InternalRequest;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Framework\Controller\StorefrontController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -52,25 +53,50 @@ class WishlistPageController extends StorefrontController
     }
 
     /**
-     * @Route("/wishlist/add/{articleId}", name="frontend.wishlist.add.modal", options={"seo"="false"}, methods={"GET"})
+     * @Route("/wishlist/modal/{articleId}", name="frontend.wishlist.add.modal", options={"seo"="false"}, methods={"GET"})
      *
-     * @throws CustomerNotLoggedInExceptionAlias
      */
-    public function add(string $articleId, InternalRequest $request, SalesChannelContext $context): Response
+    public function modal(string $articleId, InternalRequest $request, SalesChannelContext $context): Response
     {
-        $user = $context->getCustomer();
+        $user   = $context->getCustomer();
+        $product= ['name' => "ProductName"]; // @TODO: Get Article by ID
+        $lists  = [];
 
-        // Receive Wishlists of User
         if ( $user ) {
-            $lists = [1,2,3,4,5];
-            // $this->addressPageLoader->load($request, $context);
+            $lists = [
+                ['id' => '13dfns', 'name' => 'Meine Wunschliste', 'articleCount' => 3],
+                ['id' => '31vfs2', 'name' => 'Birthday', 'articleCount' => 3],
+                ['id' => 'gsdf33', 'name' => 'Wedding', 'articleCount' => 13],
+            ]; // @TODO: Get wishlists by $user
         };
 
-
-        return $this->renderStorefront('@WorkshopWishlist/page/wishlist/add.html.twig', [
+        return $this->renderStorefront('@WorkshopWishlist/page/wishlist/modal.html.twig', [
             'loggedIn' => (!empty($user)),
-            'lists' => $lists
+            'lists' => $lists,
+            'product' => $product
         ]);
+    }
+
+    /**
+     * @Route("/wishlist/add/{articleId}/{listId}", name="frontend.wishlist.add.action", options={"seo"="false"}, methods={"GET"})
+     *
+     */
+    public function add(string $articleId, string $listId, InternalRequest $request, SalesChannelContext $context): Response
+    {
+        $user = $context->getCustomer();
+        $data = [];
+
+        try{
+            $data['result'] = NULL; // @TODO: Add $articleId to wishlist with ID $listId and userID $user->getId()
+        } catch( UserNotLoggedInException $e ){
+            $data['error'] = ['code' => 601, 'message' => 'User not logged in'];
+        } catch( WishlistNotFound $e ){
+            $data['error'] = ['code' => 602, 'message' => 'List not found'];
+        }
+
+        return new JsonResponse(
+            $data
+        );
     }
 
 }
