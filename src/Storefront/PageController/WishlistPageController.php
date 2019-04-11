2332e78c71c9ff0b1af10d334eec593631456b63
Philipp Beyer <p.beyer@onacy.de>
2019-04-11 09:46:26 +0200

    Add Controller & Modal

--- src/Storefront/PageController/WishlistPageController.php
+++ src/Storefront/PageController/WishlistPageController.php
@@ -2,14 +2,41 @@
 
 namespace Workshop\Plugin\WorkshopWishlist\Storefront\PageController;
 
+use Shopware\Core\Checkout\Cart\Exception\CustomerNotLoggedInException;
 use Shopware\Core\Framework\Routing\InternalRequest;
+use Shopware\Core\PlatformRequest;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Shopware\Storefront\Framework\Controller\StorefrontController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WishlistPageController extends StorefrontController
 {
+
+    /**
+     * @throws CustomerNotLoggedInException
+     */
+    protected function isUserLoggedIn(): bool 
+    {
+        /** @var RequestStack $requestStack */
+        $requestStack = $this->get('request_stack');
+        $request = $requestStack->getMasterRequest();
+
+        if (!$request) {
+            return true;
+        }
+
+        /** @var SalesChannelContext|null $context */
+        $context = $request->attributes->get(PlatformRequest::ATTRIBUTE_STOREFRONT_CONTEXT_OBJECT);
+
+        if ($context && $context->getCustomer() && $context->getCustomer()->getGuest() === false) {
+            return true;
+        }
+
+        return false;
+    }
+
     /**
      * @Route("/wishlist", name="frontend.wishlist.list", methods={"GET"})
      *
@@ -26,4 +53,23 @@ class WishlistPageController extends StorefrontController
 
         return $this->renderStorefront('@WorkshopWishlist/page/wishlist/index.html.twig');
     }
+
+    /**
+     * @Route("/wishlist/add/{articleId}", name="frontend.wishlist.add.modal", options={"seo"="false"}, methods={"GET"})
+     *
+     * @throws CustomerNotLoggedInExceptionAlias
+     */
+    public function add(string $articleId, InternalRequest $request, SalesChannelContext $context): Response
+    {
+        if( !$this->isUserLoggedIn() ){
+            die("NOT LOGGED IN");
+        };
+
+        $lists = [1,2,3,4,5];
+
+            // $this->addressPageLoader->load($request, $context);
+
+        return $this->renderStorefront('@WorkshopWishlist/page/wishlist/add.html.twig', ['lists' => $lists]);
+    }
+
 }
