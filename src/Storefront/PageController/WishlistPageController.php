4818fdbe525c291886b9b948bc0b8f6c923144f6
Philipp Beyer <p.beyer@onacy.de>
2019-04-11 12:18:04 +0200

    Delete FakeData Method

--- src/Storefront/PageController/WishlistPageController.php
+++ src/Storefront/PageController/WishlistPageController.php
@@ -111,48 +111,6 @@ class WishlistPageController extends StorefrontController
     }
 
     /**
-<<<<<<< HEAD
-     * @param string $customerId
-     *
-     * @return array
-     */
-    private function getFakeData(string $customerId): array
-    {
-        return [
-            [
-                'id'          => 'test_1',
-                'name'        => 'Test 1',
-                'customer_id' => md5((string) \rand(0, 999999)),
-                'public'      => 1,
-            ],
-            [
-                'id'          => 'test_2',
-                'name'        => 'Test 2',
-                'customer_id' => md5((string) \rand(0, 999999)),
-                'public'      => 0,
-            ],
-            [
-                'id'          => 'test_3',
-                'name'        => 'Test 3',
-                'customer_id' => md5((string) \rand(0, 999999)),
-                'public'      => 1,
-            ],
-            [
-                'id'          => 'test_4',
-                'name'        => 'Test 4',
-                'customer_id' => md5((string) \rand(0, 999999)),
-                'public'      => 0,
-            ],
-            [
-                'id'          => 'test_5',
-                'name'        => 'Test 5',
-                'customer_id' => $customerId,
-                'public'      => 0,
-            ],
-        ];
-    }
-
-    /**
      * @Route("/wishlist/modal/{productId}", name="frontend.wishlist.add.modal", options={"seo"="false"}, methods={"GET"})
      *
      */
