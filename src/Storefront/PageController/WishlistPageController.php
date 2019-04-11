28b694c8058a05b4368a90e77fc3d7e7c5bf15cc
Jan Matthiesen <jm@netinventors.de>
2019-04-11 09:42:01 +0200

    Add wishlist detail action

--- src/Storefront/PageController/WishlistPageController.php
+++ src/Storefront/PageController/WishlistPageController.php
@@ -11,6 +11,29 @@ use Symfony\Component\Routing\Annotation\Route;
 class WishlistPageController extends StorefrontController
 {
     /**
+     * @Route("/wishlist/{id}", name="frontend.wishlist.item", methods={"GET"})
+     *
+     * @param InternalRequest     $request
+     * @param SalesChannelContext $context
+     * @param string              $id
+     *
+     * @return Response
+     */
+    public function show(InternalRequest $request, SalesChannelContext $context, string $id): Response
+    {
+        $customerId = $context->getCustomer()->getId();
+
+        // TODO: Check if wishlist is public or the logged in user is the owner of the list
+        $accessDenied = true;
+
+        if (!$accessDenied) {
+            return $this->redirectToRoute('frontend.account.login.page');
+        }
+
+        return $this->renderStorefront('@WorkshopWishlist/page/wishlist/index.html.twig');
+    }
+
+    /**
      * @Route("/wishlist", name="frontend.wishlist.list", methods={"GET"})
      *
      * @param InternalRequest     $request
@@ -18,11 +41,11 @@ class WishlistPageController extends StorefrontController
      *
      * @return Response
      */
-    public function index(InternalRequest $request, SalesChannelContext $context): Response
+    public function list(InternalRequest $request, SalesChannelContext $context): Response
     {
-        // if (!$context->getCustomer()) {
-        //     return $this->redirectToRoute('frontend.account.login.page');
-        // }
+        if (!$context->getCustomer()) {
+            return $this->redirectToRoute('frontend.account.login.page');
+        }
 
         return $this->renderStorefront('@WorkshopWishlist/page/wishlist/index.html.twig');
     }
